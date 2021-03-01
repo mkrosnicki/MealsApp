e18724aa1f261c0c893e2467d1f103598c5b4f28
mkrosnicki <maciejkrosnicki@gmail.com>
2021-03-01 13:14:32 +0100

    filtering page initial setup

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,8 +1,28 @@
 import 'package:MealsApp/widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
+
+  Widget _buildSwitchListTile(String title, String description, bool currentValue, Function updateFunction) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(description),
+      value: currentValue,
+      onChanged: updateFunction,
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +31,26 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Your Filters'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Filters!'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildSwitchListTile('Gluten-free', 'Only include gluten-free meals.', _glutenFree, (val) => _glutenFree = val),
+                _buildSwitchListTile('Vegetarian', 'Only include vegetarian meals.', _vegetarian, (val) => _vegetarian = val),
+                _buildSwitchListTile('Vegan', 'Only include vegan meals.', _vegan, (val) => _vegan = val),
+                _buildSwitchListTile('Lactose-free', 'Only include lactose-free meals.', _lactoseFree, (val) => _lactoseFree = val),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
